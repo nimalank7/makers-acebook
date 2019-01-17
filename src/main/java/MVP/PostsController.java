deef93f550faf2576b5d19c7a964138f7a29bb26
Will Wayman <will.jdw@gmail.com>
2019-01-17 15:20:15 +0000

    WIP delete and update posts

--- src/main/java/MVP/PostsController.java
+++ src/main/java/MVP/PostsController.java
@@ -3,13 +3,9 @@ package MVP;
 import MVP.entity.Post;
 import MVP.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 public class PostsController {
@@ -35,6 +31,18 @@ public class PostsController {
     @PostMapping("/newPost")
     public String newPost(Post post) {
         postRepository.save(post);
+        return "redirect:/posts";
+    }
+
+    @PostMapping("/deletePost/{id}")
+    public String deletePost(@PathVariable int id) {
+        postRepository.deleteById(id);
+        return "redirect:/posts";
+    }
+
+    @PostMapping("/updatePost/{id}")
+    public String updatePost(@PathVariable int id) {
+        postRepository.save
         return "redirect:/posts";
     }
 
